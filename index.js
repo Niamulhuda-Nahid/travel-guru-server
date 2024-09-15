424acf9b8549f202e0fb464e7485a9b5d4c765cc
Nahid Praminik <niamulhuda6046@gmail.com>
2024-09-16 02:52:43 +0600

    hotel data

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = 5000;
 
 app.use(cors())
 
-const place = require('./data/place.json')
+const place = require('./data/place.json');
+const hotel = require('./data/hotel.json');
 
 app.get('/', (req, res)=>{
     res.send('Travel guru server is running')
@@ -13,6 +14,10 @@ app.get('/', (req, res)=>{
 
 app.get('/place', (req, res)=>{
     res.send(place);
+});
+
+app.get('/hotel', (req, res)=>{
+    res.send(hotel)
 })
 
 app.listen(port, ()=>{
